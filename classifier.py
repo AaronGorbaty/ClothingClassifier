71e4b0be4bb462d5f2636aa9b8db7f0403faca96
AaronGorbaty <aarongor@icloud.com>
2024-07-14 12:30:35 -0400

    91% accuracy

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 # TensorFlow and tf.keras
 import tensorflow as tf
-
+import ssl
+ssl_context = ssl.create_default_context()
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,46 +9,154 @@ import matplotlib.pyplot as plt
 
 print(tf.__version__)
 
-# Load the dataset from the local file
-path = './mnist.npz'
-with np.load(path) as data:
-    x_train, y_train = data['x_train'], data['y_train']
-    x_test, y_test = data['x_test'], data['y_test']
+fashion_mnist = tf.keras.datasets.fashion_mnist
 
-x_train, x_test = x_train / 255.0, x_test / 255.0
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
+
+#store names of clothing categories for mapping
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images.shape
+
+len(train_labels)
+
+train_labels
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+
+# display the first 25 images
+#plt.figure(figsize=(10,10))
+#for i in range(25):
+#    plt.subplot(5,5,i+1)
+#    plt.xticks([])
+#   plt.yticks([])
+#    plt.grid(False)
+#    plt.imshow(train_images[i], cmap=plt.cm.binary)
+#    plt.xlabel(class_names[train_labels[i]])
+#plt.show()
+
+
+# model
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    # explore different activation functions
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
 ])
 
-predictions = model(x_train[:1]).numpy()
-predictions
-
-tf.nn.softmax(predictions).numpy()
-# loss function
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-loss_fn(y_train[:1], predictions).numpy()
-
-#configure and compile the model
+# compilation of model- includes loss function
 model.compile(optimizer='adam',
-              loss=loss_fn,
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-#Use the Model.fit method to adjust your model parameters and minimize the loss:
-model.fit(x_train, y_train, epochs=5)
+model.fit(train_images, train_labels, epochs=10)
 
-#evaluate accuracy using a validation set
-model.evaluate(x_test,  y_test, verbose=2)
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
-#returning probability
-probability_model = tf.keras.Sequential([
-  model,
-  tf.keras.layers.Softmax()
-])
+print('\nTest accuracy:', test_acc)
 
-#test change
-probability_model(x_test[:5])
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+
+predictions[0]
+
+np.argmax(predictions[0])
+
+#test to see if resulting label is equivalent to the test labels
+test_labels[0]
+
+# plotting functions to graph the full set of 10 class predictions
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  true_label = true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+  i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  test_labels)
+plt.show()
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  test_labels)
+plt.show()
+
+# plot the first X test images their predicted labels and the true labels
+# correct predictions in blue, incorrect predictions in red
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
+# Grab an image from the test dataset.
+img = test_images[1]
+
+print(img.shape)
+
+# Add the image to a batch where it's the only member.
+img = (np.expand_dims(img,0))
+
+print(img.shape)
+
+predictions_single = probability_model.predict(img)
+
+print(predictions_single)
+
+plot_value_array(1, predictions_single[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+
+
+np.argmax(predictions_single[0])
